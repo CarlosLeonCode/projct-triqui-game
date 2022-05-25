70de04b530b5b10827402b7c18c1f4120907bb71
Carlos Leon <carlosestebanleonpinilla@gmail.com>
2022-05-25 17:08:18 -0500

    added images, start game functionality. Modal added

--- src/js/index.js
+++ src/js/index.js
@@ -9,21 +9,61 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    class ImageNode{
+        constructor(imgName, nodeToAppend){
+            this.imgDogPath = './src/img/dog.png'
+            this.imgStarPath = './src/img/gold-star.png'
+            this.imgName = imgName
+            this.nodeToAppend = nodeToAppend
+            this.buildNode()
+            this.append()
+            this.show()
+        }
+
+        buildNode(){
+            this.imgNode = document.createElement('img')
+            if(this.imgName === "dog"){
+                this.imgNode.src = this.imgDogPath
+            }
+            if(this.imgName === "star"){
+                this.imgNode.src = this.imgStarPath
+            }
+            this.imgNode.classList.add('imgInFace')
+        }
+
+        append(){
+            this.nodeToAppend.appendChild(this.imgNode)
+        }
+
+        show(){
+            setTimeout((e) => {
+                this.imgNode.classList.add('show')
+            },100)
+        }   
+    }
+
     class Game{
-        constructor(board){
+        constructor(board, form_plyr_settings){
             this.board = board
             this.grid = [[],[],[]]
+            this.form_plyr_settings = form_plyr_settings
             this.init()
         }
 
         init(){
             this.get_faces()
-            // this.ask_player_name()
-            // this.ask_player_role
             this.add_faces_clck_listener()
+            this.set_player_settings()
             this.set_game_turn()
             this.audioFxs = new SongFx()
-            console.log(this.game_turn)
+            this.audioFxs.start.play()
+        }
+
+        set_circle_on_face(){
+        }
+
+        set_x_on_face(){
+            
         }
 
         set_game_turn(){
@@ -31,14 +71,26 @@ document.addEventListener('DOMContentLoaded', () => {
             this.game_turn = rand == 1 ? 'player' : 'machine' 
         }
 
-        ask_player_name = () => this.player_name = prompt('Ingresa tú nickname! ')
 
         get_faces(){
             this.faces = this.board.querySelectorAll('div')
         }
 
+        set_player_settings(){
+            this.player_nickname = this.form_plyr_settings.querySelector('input[name="nickname"]').value
+            this.player_character = null
+            const characters = this.form_plyr_settings.querySelectorAll('input[type="radio"]')
+            // find character checked
+            characters.forEach(node => {
+                if(node.checked){
+                    this.player_character = node.value
+                }
+            })
+        }
+
         fill_face(event){
             this.audioFxs.click.play()
+            new ImageNode('star', event.target)
         }
 
         add_faces_clck_listener(){
@@ -49,5 +101,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const board = document.querySelector('.board')   
-    const game = new Game(board)
+    const form_plyr_settings = document.getElementById('playerSettingsForm')
+    const btnCloseModal = document.getElementById('btnCloseModal')
+    
+
+    form_plyr_settings.addEventListener('submit', (e) => {
+        e.preventDefault()
+        console.log('new game created!')        
+        const game = new Game(board, form_plyr_settings)
+        btnCloseModal.click()
+    })
 })
